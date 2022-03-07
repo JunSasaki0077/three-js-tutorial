02e30a30f8cf3a00d1661dee8a2b3e5059ae39d4
Junsasaki0077 <ikasas0077@gmail.com>
2022-03-07 23:27:44 +0900

    シーン,カメラ,レンダラー,,ボックスサイズを追加

---
+++ main.js
@@ -0,0 +1,23 @@
+// シーン
+const scene = new THREE.scene();
+
+// カメラ
+const camera = new THREE.PerspectiveCamera(
+  45,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+// レンダラー
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+// ボックスのサイズ決定、メッシュ、追加
+const geometry = new THREE.BOXGeometry(2, 2, 2);
+const material = new THREE.MeshBasicMaterial({ color: 0x000ff });
+const cube = new THREE.Mesh(geometry, material);
+scene.ss(cube);
