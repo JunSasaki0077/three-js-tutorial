c10397bbcc08bf18ac3accd37da114eb473089d5
Junsasaki0077 <ikasas0077@gmail.com>
2022-03-07 23:47:27 +0900

    完成

--- main.js
+++ main.js
@@ -1,23 +1,56 @@
-// シーン
-const scene = new THREE.scene();
+let scene, camera, renderer, cube;
 
-// カメラ
-const camera = new THREE.PerspectiveCamera(
-  45,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
+function init() {
+  // シーン
+  scene = new THREE.Scene();
 
-// レンダラー
-const renderer = new THREE.WebGLRenderer();
+  // カメラ
+  camera = new THREE.PerspectiveCamera(
+    65,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
 
-renderer.setSize(window.innerWidth, window.innerHeight);
+  // レンダラー
+  renderer = new THREE.WebGLRenderer({ antialias: true });
 
-document.body.appendChild(renderer.domElement);
+  renderer.setSize(window.innerWidth, window.innerHeight);
 
-// ボックスのサイズ決定、メッシュ、追加
-const geometry = new THREE.BOXGeometry(2, 2, 2);
-const material = new THREE.MeshBasicMaterial({ color: 0x000ff });
-const cube = new THREE.Mesh(geometry, material);
-scene.ss(cube);
+  document.body.appendChild(renderer.domElement);
+
+  // ボックスのサイズ決定、メッシュ、追加
+  const geometry = new THREE.BoxGeometry(2, 2, 2);
+  // const material = new THREE.MeshBasicMaterial({ color: 0x000ff });
+
+  const texture = new THREE.TextureLoader().load(
+    "./textures/minecraft-box.png"
+  );
+  const material = new THREE.MeshBasicMaterial({ map: texture });
+  cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+
+  camera.position.z = 5;
+}
+// アニメーション制御
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+}
+
+// ウィンドウ変更時にサイズを維持する処理
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectinMatrix();
+  renderer.setSize(window, innerWidth, window.innerHeight);
+}
+
+window.addEventListener("resize", onWindowResize);
+
+init();
+animate();
